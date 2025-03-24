4554be47a547d2413906cf679e91d453dc9d3a82
Rashen Anupama <mn4raisan@gmail.com>
2025-03-24 23:00:42 +0530

    Update Part-03-(Add_button_and_funtions).py

--- Part-03-(Add_button_and_funtions).py
+++ Part-03-(Add_button_and_funtions).py
@@ -12,6 +12,7 @@ label1 = Label(root, text='Hellow World!')
 label2 = Label(root, text='My name is Rashen')
 
 button1 = Button(root, text='Click Here', command=click, padx=5, pady=2, fg='blue', bg='yellow')
+# command = function name without ()
 button1.pack()
 
 root.mainloop() # ending
