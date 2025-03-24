0d118b4dfefc39752c43383c1a59b737d3d9cf76
Rashen Anupama <mn4raisan@gmail.com>
2025-03-24 21:08:25 +0530

    Add files via upload

---
+++ Part-02-(Add_button_and_funtions).py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+root = Tk() # starting
+
+# normal function calling with (), with command no () using
+def click():
+    labelb = Label(root, text='Clicked..!')
+    labelb.pack()
+
+# Make label
+label1 = Label(root, text='Hellow World!')
+label2 = Label(root, text='My name is Rashen')
+
+button1 = Button(root, text='Click Here', command=click, padx=5, pady=2, fg='blue', bg='yellow')
+button1.pack()
+
+root.mainloop() # ending
