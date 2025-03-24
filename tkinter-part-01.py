408297be4db8cbd5c1461ddd2250b099b4a40c87
Rashen Anupama <mn4raisan@gmail.com>
2025-03-24 12:35:04 +0530

    Add files via upload

---
+++ tkinter-part-01.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk() # starting
+
+# Make label: everything have 2 parts
+label1 = Label(root, text='Hellow World!')
+
+# only space wanted, center at every size
+label1.pack()
+
+root.mainloop() # ending
