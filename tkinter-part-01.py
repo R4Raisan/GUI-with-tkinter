b518e76b5ad3ee0fd9795d3bc6f35c7f27a3982c
Rashen Anupama <mn4raisan@gmail.com>
2025-03-24 14:10:31 +0530

    Update tkinter-part-01.py

--- tkinter-part-01.py
+++ tkinter-part-01.py
@@ -2,8 +2,10 @@ from tkinter import *
 
 root = Tk() # starting
 
-# Make label: everything have 2 parts
+# Make label
 label1 = Label(root, text='Hellow World!')
+
+# everything have 2 parts : making & displaying 
 
 # only space wanted, center at every size
 label1.pack()
