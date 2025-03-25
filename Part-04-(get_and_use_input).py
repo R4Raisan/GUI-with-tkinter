a165b91639dba65bafb365ae29b8e08cc24e2890
Rashen Anupama <mn4raisan@gmail.com>
2025-03-25 11:06:15 +0530

    Add insert

--- Part-04-(get_and_use_input).py
+++ Part-04-(get_and_use_input).py
@@ -4,6 +4,11 @@ root = Tk() # starting
 
 # Just create the Input Box  |  # can't use  hight
 e = Entry(root, borderwidth=10, width=50, bg='green', fg='black')
+
+# Add text into input box - Prefilled Input Box
+# But u have to clear and give the input, else it's on the output
+e.insert(0, 'Enter your name: ')
+
 e.pack()
 
 e.get() # input for using 
