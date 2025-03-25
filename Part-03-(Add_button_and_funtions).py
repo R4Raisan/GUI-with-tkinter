a5760f012fc7aff8ced9e096abd9ab76192a5026
Rashen Anupama <mn4raisan@gmail.com>
2025-03-25 09:47:39 +0530

    Fix Part-03-(Add_button_and_funtions).py

--- Part-03-(Add_button_and_funtions).py
+++ Part-03-(Add_button_and_funtions).py
@@ -7,10 +7,7 @@ def click():
     labelb = Label(root, text='Clicked..!')
     labelb.pack()
 
-# Make label
-label1 = Label(root, text='Hellow World!')
-label2 = Label(root, text='My name is Rashen')
-
+# add button
 button1 = Button(root, text='Click Here', command=click, padx=5, pady=2, fg='blue', bg='yellow')
 # command = function name without ()
 button1.pack()
