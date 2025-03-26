1ef72be1f1508cad54e7f1864271c2c53b7c40f8
Rashen Anupama <mn4raisan@gmail.com>
2025-03-26 22:28:46 +0530

    Fix lambda & more clearence

--- Part-03-(Add_button_and_funtions).py
+++ Part-03-(Add_button_and_funtions).py
@@ -12,11 +12,14 @@ button1 = Button(root, text='Click Here', command=click, padx=5, pady=2, fg='blu
 # command = function name without ()
 button1.pack()
 
+label_or = Label(root, text='OR')
+label_or.pack() # just for clearence
+
 def press(i):
-    label_2 = Label(root, text=5+i)
+    label_2 = Label(root, text=5+i, border=3)
     label_2.pack()
 
-button_2 = Button(root, text='5+7', command=Lambda: press(7) )
+button_2 = Button(root, text="What's 5+7 ?", command=lambda: press(7), border=6, bg='cyan')
 button_2.pack()
 
 root.mainloop() # ending
