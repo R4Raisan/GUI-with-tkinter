1578bea951572fd49d6b8da71bc40b371c303b3c
Rashen Anupama <mn4raisan@gmail.com>
2025-03-26 22:42:43 +0530

    add the delete funtion with input box

--- Part-04-(get_and_use_input).py
+++ Part-04-(get_and_use_input).py
@@ -7,7 +7,9 @@ e = Entry(root, borderwidth=10, width=50, bg='green', fg='black')
 
 # Add text into input box - Prefilled Input Box
 # But u have to clear and give the input, else it's on the output
+
 e.insert(0, 'Enter your name: ')
+#e.delete(0, END) # clear the whole input box
 
 e.pack()
 
@@ -16,6 +18,11 @@ e.get() # input for using
 def click():
     label1 = Label(root, text='Hi '+e.get()+'...!') # e.get()
     label1.pack()
+    e.delete(0, END) # clear the box forever after the button clicked
+    label2 = Label(root, text='Hi '+e.get()+'...!')
+    label2.pack()
+    # back to normal, above delete deleted forever
+    e.insert(0, 'Enter your name: ')
 
 button1 = Button(root, text='Submit', command=click)
 button1.pack()
