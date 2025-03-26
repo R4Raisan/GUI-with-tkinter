fa46eb64e50b0e29acaa3ab01b17d521e3861b19
Rashen Anupama <mn4raisan@gmail.com>
2025-03-26 22:13:01 +0530

    add rowspan and columnspan

--- Part-02-(grid).py
+++ Part-02-(grid).py
@@ -3,12 +3,20 @@ from tkinter import *
 root = Tk()
 
 # Make labels
-label1 = Label(root, text='Hellow World!')
-label2 = Label(root, text='My name is Rashen')
+label1 = Label(root, text='1')
+label2 = Label(root, text='columnspan')
+label3 = Label(root, text='2')
+label4 = Label(root, text='2i')
+label5 = Label(root, text='rowspan')
+label6 = Label(root, text='I am here')
 
 # stay stable, spacing as using
 label1.grid(row=0, column=0) # also like label1 = Label(root, text='Hellow World!').grid(row=0, column=0)
-label2.grid(row=5, column=7) 
+label2.grid(row=0, column=1, columnspan=2) 
+label3.grid(row=1, column=0)
+label4.grid(row=2, column=0)
+label5.grid(row=1, column=2, rowspan=2)
+label6.grid(row=10, column=7, columnspan=5) # emptys not counted
 # no matter which is the grid value, because empty grids are don't display in the output
 
 root.mainloop()
