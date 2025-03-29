9f3f5c31b0220d1c869d1a6c6200b6724d200bcc
Rashen Anupama <mn4raisan@gmail.com>
2025-03-29 22:16:02 +0530

    Simple the code and add explanations

--- Part-05-(Simple_Calculator).py
+++ Part-05-(Simple_Calculator).py
@@ -14,60 +14,61 @@ e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 i="" # full empty value to veriable
 
 # process
-def whats(t):
-    global i
-    i = i+t
 
-def result():
+def result():  # show the final result
     ans = eval(i)
     e.delete(0, END)
     e.insert(0, str(ans))
 
 def typing(t):
-    et = e.get()
+
+    global i  # undate the i globally i for last result
+    i = i+t
+
+    et = e.get()  # input box value handling
     e.delete(0, END)
     e.insert(0, et+str(t))
 
-def clearf():
+def clearf():  # totally clear the input box
     e.delete(0, END)
-    global i
+    global i 
     i=""
     
 # buttons with positions
-button0 = Button(root, text='0', command=lambda: (typing(0), whats('0')), padx=20, pady=10)
+button0 = Button(root, text='0', command=lambda: (typing('0')), padx=20, pady=10)
 button0.grid(row=2, column=0)
 
-button1 = Button(root, text='1', command=lambda: (typing(1), whats('1')), padx=20, pady=10)
+button1 = Button(root, text='1', command=lambda: (typing('1')), padx=20, pady=10)
 button1.grid(row=2, column=1)
 
-button2 = Button(root, text='2', command=lambda: (typing(2), whats('2')), padx=20, pady=10)
+button2 = Button(root, text='2', command=lambda: (typing('2')), padx=20, pady=10)
 button2.grid(row=2, column=2)
 
-button3 = Button(root, text='3', command=lambda: (typing(3), whats('3')), padx=20, pady=10)
+button3 = Button(root, text='3', command=lambda: (typing('3')), padx=20, pady=10)
 button3.grid(row=3, column=0)
 
-button4 = Button(root, text='4', command=lambda: (typing(4), whats('4')), padx=20, pady=10)
+button4 = Button(root, text='4', command=lambda: (typing('4')), padx=20, pady=10)
 button4.grid(row=3, column=1)
 
-button5 = Button(root, text='5', command=lambda: (typing(5), whats('5')), padx=20, pady=10)
+button5 = Button(root, text='5', command=lambda: (typing('5')), padx=20, pady=10)
 button5.grid(row=3, column=2)
 
-button6 = Button(root, text='6', command=lambda: (typing(6), whats('6')), padx=20, pady=10)
+button6 = Button(root, text='6', command=lambda: (typing('6')), padx=20, pady=10)
 button6.grid(row=4, column=0)
 
-button7 = Button(root, text='7', command=lambda: (typing(7), whats('7')), padx=20, pady=10)
+button7 = Button(root, text='7', command=lambda: (typing('7')), padx=20, pady=10)
 button7.grid(row=4, column=1)
 
-button8 = Button(root, text='8', command=lambda: (typing(8), whats('8')), padx=20, pady=10)
+button8 = Button(root, text='8', command=lambda: (typing('8')), padx=20, pady=10)
 button8.grid(row=4, column=2)
 
-button9 = Button(root, text='9', command=lambda: (typing(9), whats('9')), padx=20, pady=10)
+button9 = Button(root, text='9', command=lambda: (typing('9')), padx=20, pady=10)
 button9.grid(row=5, column=0)
 
-buttonadd = Button(root, text='+', command=lambda: (typing('+'), whats('+')), padx=20, pady=10)
+buttonadd = Button(root, text='+', command=lambda: (typing('+')), padx=20, pady=10)
 buttonadd.grid(row=5, column=1)
 
-buttonmns = Button(root, text='-', command=lambda: (typing('-'), whats('-')), padx=20, pady=10)
+buttonmns = Button(root, text='-', command=lambda: (typing('-')), padx=20, pady=10)
 buttonmns.grid(row=5, column=2)
 
 buttonclr = Button(root, text='C', command=clearf, padx=20, pady=10)
