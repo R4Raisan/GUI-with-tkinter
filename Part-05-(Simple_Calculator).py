00994a31dd86f3ce784aa9c09e3d8560b5a5754f
Rashen Anupama <mn4raisan@gmail.com>
2025-04-01 21:53:43 +0530

    Improve numbers clearity

--- Part-05-(Simple_Calculator).py
+++ Part-05-(Simple_Calculator).py
@@ -61,34 +61,34 @@ def clearf():  # totally clear the input box
     
 
 # buttons with positions
-button0 = Button(root, text='0', command=lambda: (typing('0')), padx=20, pady=10)
+button0 = Button(root, text='0', command=lambda: (typing('0')), padx=20, pady=10, bg='#FFDE21')
 button0.grid(row=1, column=0)
 
-button1 = Button(root, text='1', command=lambda: (typing('1')), padx=20, pady=10)
+button1 = Button(root, text='1', command=lambda: (typing('1')), padx=20, pady=10, bg='#FFDE21')
 button1.grid(row=1, column=1)
 
-button2 = Button(root, text='2', command=lambda: (typing('2')), padx=20, pady=10)
+button2 = Button(root, text='2', command=lambda: (typing('2')), padx=20, pady=10, bg='#FFDE21')
 button2.grid(row=1, column=2)
 
-button3 = Button(root, text='3', command=lambda: (typing('3')), padx=20, pady=10)
+button3 = Button(root, text='3', command=lambda: (typing('3')), padx=20, pady=10, bg='#FFDE21')
 button3.grid(row=1, column=3)
 
-button4 = Button(root, text='4', command=lambda: (typing('4')), padx=20, pady=10)
+button4 = Button(root, text='4', command=lambda: (typing('4')), padx=20, pady=10, bg='#FFDE21')
 button4.grid(row=2, column=0)
 
-button5 = Button(root, text='5', command=lambda: (typing('5')), padx=20, pady=10)
+button5 = Button(root, text='5', command=lambda: (typing('5')), padx=20, pady=10, bg='#FFDE21')
 button5.grid(row=2, column=1)
 
-button6 = Button(root, text='6', command=lambda: (typing('6')), padx=20, pady=10)
+button6 = Button(root, text='6', command=lambda: (typing('6')), padx=20, pady=10, bg='#FFDE21')
 button6.grid(row=2, column=2)
 
-button7 = Button(root, text='7', command=lambda: (typing('7')), padx=20, pady=10)
+button7 = Button(root, text='7', command=lambda: (typing('7')), padx=20, pady=10, bg='#FFDE21')
 button7.grid(row=2, column=3)
 
-button8 = Button(root, text='8', command=lambda: (typing('8')), padx=20, pady=10)
+button8 = Button(root, text='8', command=lambda: (typing('8')), padx=20, pady=10, bg='#FFDE21')
 button8.grid(row=3, column=0)
 
-button9 = Button(root, text='9', command=lambda: (typing('9')), padx=20, pady=10)
+button9 = Button(root, text='9', command=lambda: (typing('9')), padx=20, pady=10, bg='#FFDE21')
 button9.grid(row=3, column=1)
 
 buttonclr = Button(root, text='C', command=clearf, padx=19, pady=10)
