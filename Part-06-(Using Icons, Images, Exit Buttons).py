d70ee963c7ffc722bc2647c9c71bdeae40460975
Rashen Anupama <mn4raisan@gmail.com>
2025-04-04 01:39:11 +0530

    add extra description

--- Part-06-(Using Icons, Images, Exit Buttons).py
+++ Part-06-(Using Icons, Images, Exit Buttons).py
@@ -4,5 +4,6 @@ from PIL import ImageTk,Image   # pip install Pillow - for images
 root = Tk()
 root.title('Icons & Images & Exit Program')
 
+# make separate label for each picture
 
 root.mainloop()
