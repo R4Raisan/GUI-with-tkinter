37f5a1441224d0658b4c36d5647f5b0089067795
Rashen Anupama <mn4raisan@gmail.com>
2025-04-05 00:51:08 +0530

    add picture

--- Part-06-(Using Icons, Images, Exit Buttons).py
+++ Part-06-(Using Icons, Images, Exit Buttons).py
@@ -7,4 +7,7 @@ root.title('Icons & Images & Exit Program')
 # add the window icon
 root.iconbitmap('Part-06-(iconbitmap).ico')
 
+# add picture
+img1 = ImageTk.PhotoImage(Image.open('Part-06-(Image).png'))
+
 root.mainloop()
