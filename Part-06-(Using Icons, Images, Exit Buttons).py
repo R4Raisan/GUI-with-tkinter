0c15e0e8ff8c157bc8c16fb676612f9d1c8cdacc
Rashen Anupama <mn4raisan@gmail.com>
2025-04-04 22:15:09 +0530

    Add the icon

--- Part-06-(Using Icons, Images, Exit Buttons).py
+++ Part-06-(Using Icons, Images, Exit Buttons).py
@@ -4,5 +4,8 @@ from PIL import ImageTk,Image   # pip install Pillow - for images
 root = Tk()
 root.title('Icons & Images & Exit Program')
 
+# add the window icon
+root.iconbitmap('Part-06-(iconbitmap).ico')
+
 
 root.mainloop()
