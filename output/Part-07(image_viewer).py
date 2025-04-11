ec68f3935d4497b3e72628289642d0a6358eb116
Rashen Anupama <mn4raisan@gmail.com>
2025-04-11 22:20:25 +0530

    add structure

--- output/Part-07(image_viewer).py
+++ output/Part-07(image_viewer).py
@@ -1,1 +1,5 @@
 # will continue as a seperate project
+
+# while using tkinter,
+''' we need to add the images as labels for each image.
+back and forward buttons can be written as list or for or indexing'''
